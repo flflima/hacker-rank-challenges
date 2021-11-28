4e722150d135c58475f1de09d103f93e26a362d1
Felipe Lima <flf.felipe@gmail.com>
2021-11-28 13:18:00 -0300

    Treats first half hour

--- the-time-in-words/src/main/kotlin/br/com/dev/TimeInWords.kt
+++ the-time-in-words/src/main/kotlin/br/com/dev/TimeInWords.kt
@@ -1,6 +1,6 @@
 package br.com.dev
 
-val hourStr = mapOf(
+val numerals = mapOf(
     1 to "one",
     2 to "two",
     3 to "three",
@@ -12,11 +12,37 @@ val hourStr = mapOf(
     9 to "nine",
     10 to "ten",
     11 to "eleven",
-    12 to "twelve"
+    12 to "twelve",
+    13 to "thirteen",
+    14 to "fourteen",
+    15 to "fifteen",
+    16 to "sixteen",
+    17 to "seventeen",
+    18 to "eighteen",
+    19 to "nineteen",
+    20 to "twenty",
+    30 to "thirty",
+    40 to "forty",
+    50 to "sixty",
+    60 to "fifty",
 )
 
-val complement = mapOf(0 to "o'clock")
-
 fun timeInWords(hour: Int, minutes: Int): String {
-    return "${hourStr[hour]} ${complement[minutes]}"
+    val hourStr = numerals[hour]
+    val minuteStr = toMinutesString(minutes)
+    if (minutes == 1) return "$minuteStr minute past $hourStr"
+    if (minutes == 30) return "half past $hourStr"
+    if (minutes == 15) return "quarter past $hourStr"
+    if (minutes in 2..14 || minutes in 16..29) return "$minuteStr minutes past $hourStr"
+    return "$hourStr o'clock"
+}
+
+fun toMinutesString(minutes: Int): String {
+    if (minutes == 0) return ""
+    if (minutes in 1..19) return numerals[minutes]!!
+    if (minutes % 10 == 0) return numerals[minutes]!!
+
+    val minuteUnity = minutes % 10
+    val minuteDozen = minutes - minuteUnity
+    return "${numerals[minuteDozen]} ${numerals[minuteUnity]}"
 }
--- the-time-in-words/src/test/kotlin/br/com/dev/TimeInWordsKtTest.kt
+++ the-time-in-words/src/test/kotlin/br/com/dev/TimeInWordsKtTest.kt
@@ -18,4 +18,39 @@ internal class TimeInWordsKtTest {
 
         assertThat(timeInWords).isEqualTo("five o'clock")
     }
+
+    @Test
+    fun `given time 2h01 should return "one minute past two"`() {
+        val timeInWords = timeInWords(2, 1)
+
+        assertThat(timeInWords).isEqualTo("one minute past two")
+    }
+
+    @Test
+    fun `given time 5h10 should return "ten minutes past five"`() {
+        val timeInWords = timeInWords(5, 10)
+
+        assertThat(timeInWords).isEqualTo("ten minutes past five")
+    }
+
+    @Test
+    fun `given time 6h15 should return "quarter past six"`() {
+        val timeInWords = timeInWords(6, 15)
+
+        assertThat(timeInWords).isEqualTo("quarter past six")
+    }
+
+    @Test
+    fun `given time 6h28 should return "twenty eight minutes past six"`() {
+        val timeInWords = timeInWords(6, 28)
+
+        assertThat(timeInWords).isEqualTo("twenty eight minutes past six")
+    }
+
+    @Test
+    fun `given time 3h30 should return "half past three"`() {
+        val timeInWords = timeInWords(3, 30)
+
+        assertThat(timeInWords).isEqualTo("half past three")
+    }
 }
