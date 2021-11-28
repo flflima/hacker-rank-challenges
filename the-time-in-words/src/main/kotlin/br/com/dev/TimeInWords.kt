94e6a11e73b30914d36184b538111ba7036a1a2e
Felipe Lima <flf.felipe@gmail.com>
2021-11-28 12:41:08 -0300

    convert hours to map

--- the-time-in-words/src/main/kotlin/br/com/dev/TimeInWords.kt
+++ the-time-in-words/src/main/kotlin/br/com/dev/TimeInWords.kt
@@ -1,15 +1,24 @@
 package br.com.dev
 
+val hourStr = mapOf(
+    1 to "one",
+    2 to "two",
+    3 to "three",
+    4 to "four",
+    5 to "five",
+    6 to "six",
+    7 to "seven",
+    8 to "eight",
+    9 to "nine",
+    10 to "ten",
+    11 to "eleven",
+    12 to "twelve"
+)
+
 fun timeInWords(hour: Int, minutes: Int): String {
-    var hourString = ""
     var minutesString = ""
-    if (hour == 1) {
-        hourString = "one"
-    } else if (hour == 5) {
-        hourString = "five"
-    }
     if (minutes == 0) {
         minutesString = "o'clock"
     }
-    return "$hourString $minutesString"
+    return "${hourStr[hour]} $minutesString"
 }
